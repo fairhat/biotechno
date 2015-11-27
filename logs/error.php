9519242003d80ec31df52359a856e2d743d2122e
fearhat <topcu.ft@gmail.com>
2015-11-27 16:32:41 +0100

    added joomla

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-27 14:57:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-11-27T14:57:18+00:00	INFO 160.45.6.205	joomlafailure	Username and password do not match or you do not have an account yet.
